d103fdf3ecc94eee92d1e36af283c1ae45493610
Lucas Queiroz <lucas2010.97@gmail.com>
2022-10-12 01:23:35 -0300

    commit

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask
+import requests
+import json
+import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def pagina_inicial():
-    return render_template("index.html")
+    request = requests.get("https://state-train-sp.herokuapp.com/")
+    data = json.loads(request.content)
+    dt = pd.json_normalize(data) 
+    html = dt.to_html()
+    return html
 
 if __name__ == '__main__':
     app.run()
