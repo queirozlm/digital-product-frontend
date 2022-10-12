56f4aaec055e764cec985093d864cb9878cae2b5
Lucas Queiroz <lucas2010.97@gmail.com>
2022-10-12 00:49:17 -0300

    quatro

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def pagina_inicial():
-    return "teste Lucas Martins"
+    return "<h1>olá mundo</<h1>"
 
 if __name__ == '__main__':
     app.run()
