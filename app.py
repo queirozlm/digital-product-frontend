52f808e9f20ecf3d5e8a7931220e70d350498c8b
Lucas Queiroz <lucas2010.97@gmail.com>
2022-10-12 00:55:12 -0300

    teste5

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def pagina_inicial():
-    return "<h1>olá mundo</<h1>"
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()
